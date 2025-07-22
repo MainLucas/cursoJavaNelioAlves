34748e4a7a7ab579bb3890e1a022a255163360c9
Lucas-PCtrabalho <mainn.lucas@gmail.com>
2025-07-22 17:50:25 -0300

    Realizado aula Estrutura de repetição while - WORK

--- CursoJava/src/main/java/com/cursoJavaModulos/secao06EstruturaRepetitiva/AestruturaWhile/tests/Test01.java
+++ CursoJava/src/main/java/com/cursoJavaModulos/secao06EstruturaRepetitiva/AestruturaWhile/tests/Test01.java
@@ -1,4 +1,19 @@
 package com.cursoJavaModulos.secao06EstruturaRepetitiva.AestruturaWhile.tests;
 
+import java.util.Scanner;
+
 public class Test01 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int soma = 0;
+        while (x != 0){
+            soma += x;
+            System.out.println("Digite novamente: ");
+            x = sc.nextInt();
+
+        }
+        System.out.printf("O valor de x é: %d", soma);
+        sc.close();
+    }
 }
